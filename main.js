97cc0ec2873a80cd48f01bd3a0ce062da10cb4e2
k33g <k33g@github.com>
2016-11-05 10:08:17 +0100

    Gardening ...

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const Human = require('./libs/Human').Human
+const sayHello = require('./libs/sayhello')
+const sayBye = require('./libs/saybye')
+
 
 let bob = new Human({
   firstName: "Bob",
   lastName: "Morane"
-});
+}, sayHello, sayBye);
 
 bob.sayHi();
+bob.sayHello();
+bob.sayBye();
